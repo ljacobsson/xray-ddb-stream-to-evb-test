59ddb752612e73758fc40aae7057d88a1a408fb9
ljacobsson <lars@mathem.se>
2022-07-12 14:06:34 +0200

    first commit

--- service-b/src/DynamoDbTrigger.js
+++ service-b/src/DynamoDbTrigger.js
@@ -2,15 +2,17 @@ const EventBridge = require('aws-sdk/clients/eventbridge');
 const eventBridgeClient = new EventBridge();
 
 exports.handler = async (event) => {
-  const traceId = event.Records[0].dynamodb.NewImage._traceId.S;
-  await eventBridgeClient.putEvents({
-    Entries: [
-      {
-        Source: "service-b",
-        DetailType: "DynamoDbTrigger",
-        Detail: JSON.stringify(event.Records[0].dynamodb.NewImage),
-        TraceHeader: traceId,
-        EventBusName: "custom"
-      }]
-  }).promise();
+  for (const record of event.Records) {
+    const traceId = record.dynamodb.NewImage._traceId.S;
+    await eventBridgeClient.putEvents({
+      Entries: [
+        {
+          Source: "service-b",
+          DetailType: "DynamoDbTrigger",
+          Detail: JSON.stringify(record.dynamodb.NewImage),
+          TraceHeader: traceId,
+          EventBusName: "custom"
+        }]
+    }).promise();
+  }
 }
