e18b4a3ff574365ea76b79cc4260ebe7f16730fb
ljacobsson <lars@mathem.se>
2022-07-12 14:12:41 +0200

    first commit

--- service-b/src/DynamoDbTrigger.js
+++ service-b/src/DynamoDbTrigger.js
@@ -4,6 +4,7 @@ const eventBridgeClient = new EventBridge();
 exports.handler = async (event) => {
   for (const record of event.Records) {
     const traceId = record.dynamodb.NewImage._traceId.S;
+    process.env._X_AMZN_TRACE_ID = traceId;
     await eventBridgeClient.putEvents({
       Entries: [
         {
